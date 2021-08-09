ee0697b01fc4aca11c14cb2d63bc9eb7725f7182
salvatorebuono02 <salvatore.buono@mail.polimi.it>
2021-08-09 21:01:59 +0200

    begin of add graph

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
-#define MAXBUFFER 10
+#define MAXBUFFER 100
+#define MAXWEIGHT 100
+void addGraph(int);
+
 int main() {
 
     char buffer[MAXBUFFER];
@@ -10,9 +13,10 @@ int main() {
     int i;
     int j=0;
     int n;
-
+    char addgraph[MAXBUFFER]="AggiungiGrafo";
     int k;
 
+    //lettura prima riga
     firstRow=(char *) malloc(sizeof(char )*MAXBUFFER);
     numOfNodes=(char *) calloc(MAXBUFFER,sizeof (char ));
     if(!numOfNodes){
@@ -40,5 +44,33 @@ int main() {
     free(numOfNodes);
     free(kgraph);
 
+    //gestione di aggiunta grafo
+    if(strcmp(addgraph, fgets(buffer,20,stdin))){
+        //printf("Comando inviato correttamente\n ");
+        addGraph(n);
+    }
     return 0;
 }
+
+void addGraph(int n){
+    int **g;
+    int j=0;
+    char w[MAXWEIGHT];
+    char *row;
+    char *buffer;
+    row=malloc(sizeof(int )* MAXBUFFER);
+    buffer=malloc(sizeof(int )* MAXBUFFER);
+    g=(int**) malloc(sizeof(int)*n*n);
+    for(int i=0;i<n;i++){
+        strcpy(row, fgets(buffer,MAXBUFFER,stdin));
+        for(int k=0;row[k]!=' ';k++){
+            if(row[k]!=','){
+                w[k]=row[k];
+            }
+        }
+        g[j][i]= atoi(w);
+        j++;
+    }
+    free(row);
+    free(buffer);
+    }
